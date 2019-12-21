3a306f06172ff70b9a84bfb38d1e2c2190c99b9e
hirota745 <hiroto.tate@a-saas.com>
2019-12-21 21:54:23 +0900

    Dockerイメージ名を変更した

--- fall.sh
+++ fall.sh
@@ -5,7 +5,7 @@ set -e
 open /Applications/Utilities/XQuartz.app
 /opt/X11/bin/xhost + 127.0.0.1
 
-docker image build -t fall-something:latest .
-docker run --rm -it -e DISPLAY=host.docker.internal:0 fall-something:latest $1
+docker image build -t snow-falls:latest .
+docker run --rm -it -e DISPLAY=host.docker.internal:0 snow-falls:latest $1
 
 pkill "launchd_startx"
